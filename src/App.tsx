12371ff1aaed867d34f9fb433cd32a7bee8906f7
Hugo <hugolacerdar@gmail.com>
2021-10-08 20:35:42 -0300

    Add custom color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Text, Button, useColorMode } from "@chakra-ui/react";
+import ColorModeSwitch from "./components/ColorModeSwitch";
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ColorModeSwitch />
+      <Text>
+        Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu
+        reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis
+        que eu levo! Suco de cevadiss deixa as pessoas mais interessantis.
+        Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+        Mé faiz elementum girarzis, nisi eros vermeio. Si u mundo tá muito
+        paradis? Toma um mé que o mundo vai girarzis! Nec orci ornare consequat.
+        Praesent lacinia ultrices consectetur. Sed non ipsum felis. Vehicula
+        non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique
+        interdum.
+      </Text>
+    </>
   );
 }
 
