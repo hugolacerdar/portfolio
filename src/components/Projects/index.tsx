459dae4848b85ac248dc751f774d878c63de6a59
Hugo <hugolacerdar@gmail.com>
2021-10-11 23:55:21 -0300

    Add Projects Components

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex } from "@chakra-ui/layout";
+import { Stack } from "@chakra-ui/react";
+import HDivider from "../shared/HDivider";
+import Project from "./Project";
+import SectionHeader from "../shared/SectionHeader";
+
+export default function Projects() {
+  return (
+    <Flex my="20" direction="column">
+      <SectionHeader sectionTitle="Portfolio" />
+      <Project />
+      <HDivider />
+      <Project />
+      <HDivider />
+      <Project />
+    </Flex>
+  );
+}
